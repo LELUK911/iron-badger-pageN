7ac21bed001e7626f7c718d1e4e56cb0e7ba133d
leluk911 <luciano.dinoia91@outlook.it>
2025-05-14 11:03:04 +0200

    remove visualizer after fleek updare broken

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({
-      filename: './dist/stats.html', // Specifica il percorso e il nome del file
-      open: true,                    // Apri automaticamente il report nel browser
-      template: 'treemap',           // Tipo di visualizzazione: 'treemap', 'sunburst', 'network'
-      gzipSize: true,                // Mostra le dimensioni gzip
-      brotliSize: true               // Mostra le dimensioni brotli
-    })
   ],
   base: './',
   optimizeDeps: {
